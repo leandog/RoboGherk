5e8c7fea56211557bc94a91a6868021c3fdb4c7d
Dave Shah <david.a.shah@gmail.com>
2012-07-16 16:37:00 -0400

    Remove old, unused findStepsFor()

--- RoboGherk/src/com/leandog/robogherk/StepFinder.java
+++ RoboGherk/src/com/leandog/robogherk/StepFinder.java
@@ -9,10 +9,11 @@ public class StepFinder {
         this.packageName = packageName;
     }
 
-    public StepDefinitions findStepsFor(String feature) throws NoStepsFoundException {
-        return loadStepsClass(feature, getClassName(feature));
+    public StepDefinitions findStepsFor(Class<? extends RoboGherkTester> testCaseClass) throws RoboGherkException {
+        String stepDefinitionClassName = testCaseClass.getSimpleName().replace("Test","Steps");
+        return loadStepsClass(testCaseClass.getSimpleName(), packageName + "." + stepDefinitionClassName);
     }
-
+    
     private StepDefinitions loadStepsClass(String feature, String className) throws NoStepsFoundException {
         StepDefinitions steps = null;
         try {
@@ -24,29 +25,4 @@ public class StepFinder {
         return steps;
     }
     
-    private String getClassName(String featureName) {
-        String[] allTheWordsInTheFeatureName = featureName.split(" ");
-        return nameOfStepsClassFrom(packageName, allTheWordsInTheFeatureName);
-    }
-
-    private String nameOfStepsClassFrom(String packageName, String[] allTheWordsInTheFeatureName) {
-        return packageName + "." + camelCase(allTheWordsInTheFeatureName) + "Steps";
-    }
-
-    private String camelCase(String[] allTheWordsInTheFeatureName) {
-        StringBuilder classNameBuilder = new StringBuilder(allTheWordsInTheFeatureName.length);
-        for (String word : allTheWordsInTheFeatureName) {
-            classNameBuilder.append(camelCaseThe(word));
-        }
-        return classNameBuilder.toString();
-    }
-
-    private String camelCaseThe(String word) {
-        return (word.charAt(0) + "").toUpperCase() + word.substring(1, word.length());
-    }
-
-    public StepDefinitions findStepsFor(Class<? extends RoboGherkTester> testCaseClass) throws RoboGherkException {
-        String stepDefinitionClassName = testCaseClass.getSimpleName().replace("Test","Steps");
-        return loadStepsClass(testCaseClass.getSimpleName(), packageName + "." + stepDefinitionClassName);
-    }
 }
--- RoboGherkUnits/src/com/leandog/test/StepFinderTest.java
+++ RoboGherkUnits/src/com/leandog/test/StepFinderTest.java
@@ -4,31 +4,12 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.leandog.robogherk.NoStepsFoundException;
 import com.leandog.robogherk.StepDefinitions;
 import com.leandog.robogherk.StepFinder;
 import com.leandog.robogherk.examples.MyFeatureTest;
 
 public class StepFinderTest {
-    
-    @Test
-    public void findsStepsForFeature() throws Exception {
-        StepDefinitions steps = new StepFinder("com.leandog.test.fake").findStepsFor("local fake");
-        String actualClassName = steps.getClass().getName();
-        assertEquals("com.leandog.test.fake.LocalFakeSteps", actualClassName);
-    }
 
-    @Test
-    public void findsStepsForAFeatureInADifferentPackage() throws Exception {
-        StepDefinitions steps = new StepFinder("com.leandog.test.fake").findStepsFor("fake");
-        assertEquals("com.leandog.test.fake.FakeSteps", steps.getClass().getName());
-    }
-
-    @Test(expected = NoStepsFoundException.class)
-    public void itLetsMeKnowWhenItCantFindSteps() throws Exception {
-        new StepFinder("com.leandog.test").findStepsFor("this is fake");
-    }
-    
     @Test
     public void findsStepsForClass() throws Exception {
         StepDefinitions steps = new StepFinder("com.leandog.test.fake").findStepsFor(MyFeatureTest.class);
