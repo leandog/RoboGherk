e4bd4427114f3c8dfbfb25bec07f372ad5df7da8
Angela Harms <angela.harms@gmail.com>
2012-08-07 16:58:14 -0400

    Don't put stuff inside the ivy <info/> because ouch

--- robogherk/add_version_to_ivy_file.rb
+++ robogherk/add_version_to_ivy_file.rb
@@ -3,7 +3,7 @@
 version = ARGV.first
 
 contents = File.open('ivy.xml','r') {|file| file.read}; 
-contents.gsub!(/(<info[^>]*)>/) { $1 + " revision = \"#{version}\">"; }
+contents.gsub!(/(<info[^>]*)(\/>)/) { $1 + " revision = \"#{version}\" " + $2; }
 File.open("bin/artifacts/ivy-#{version}.xml", 'w') do |file| 
   file.write contents
 end
