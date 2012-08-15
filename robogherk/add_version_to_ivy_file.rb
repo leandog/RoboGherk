9384ec0b4ecb3a43a1eed1f03b3fa549c99cd8fc
Jason Felice <jason.m.felice@gmail.com>
2012-08-15 13:17:30 -0400

    Remove ivy

--- robogherk/add_version_to_ivy_file.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-version = ARGV.first
-
-contents = File.open('ivy.xml','r') {|file| file.read}; 
-contents.gsub!(/(<info[^>]*)(\/>)/) { $1 + " revision = \"#{version}\" " + $2; }
-File.open("bin/artifacts/ivy-#{version}.xml", 'w') do |file| 
-  file.write contents
-end
