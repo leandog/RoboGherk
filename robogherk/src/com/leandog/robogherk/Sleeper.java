69012d46044fbf6ef42115f888a10c29b1073bbe
Jason Felice <jason.m.felice@gmail.com>
2012-08-28 16:10:49 -0400

    Implement sleep()

--- robogherk/src/com/leandog/robogherk/Sleeper.java
+++ robogherk/src/com/leandog/robogherk/Sleeper.java
@@ -3,6 +3,10 @@ package com.leandog.robogherk;
 public class Sleeper {
 
     public void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+        }
     }
 
 }
